c77b2b8d2d1c6acf5a5c189433b0953f71794160
Lola Marinkovic <lola.marinkovic@ontariotechu.net>
2020-10-29 11:13:41 -0400

    add numbers for october 29 2020

--- src/Chart.js
+++ src/Chart.js
@@ -265,7 +265,8 @@ const data = [
     {cases: 1042, date: '10-25-2020'},
     {cases: 851, date: '10-26-2020'},
     {cases: 827, date: '10-27-2020'},
-    {cases: 834, date: '10-28-2020'}
+    {cases: 834, date: '10-28-2020'},
+    {cases: 934, date: '10-29-2020'}
 ]
 
 class Chart extends React.Component {
