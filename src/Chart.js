878bc8ba6cec87264daeda1d9d23d7812f01f66c
Lola Marinkovic <lola.marinkovic@ontariotechu.net>
2020-11-01 21:15:31 -0500

    names of data files changed

--- src/Chart.js
+++ src/Chart.js
@@ -1,21 +1,38 @@
-import {caseData} from './data';
+import {caseData} from './caseData';
+import {testingData} from './testingData';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer} from 'recharts';
 
 
 class Chart extends React.Component {
     render() {
         return (
             <div>
-                <LineChart width={1000} height={500} data={caseData} margin={{top: 50, right: 50, left: 20, bottom: 5}}>
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="date" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Line type="monotone" dataKey="cases" stroke="#8884d8" />
-                </LineChart>
+                <ResponsiveContainer width="80%" height={500}>
+                    <LineChart data={caseData} margin={{top: 50, right: 50, left: 20, bottom: 5}}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="date" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Line type="monotone" dataKey="cases" stroke="#8884d8" />
+                    </LineChart>
+                </ResponsiveContainer>
+
+                <ResponsiveContainer width="80%" height={500}>
+                    <LineChart data={testingData} margin={{top: 50}} >
+                       <CartesianGrid strokeDasharray="3 3" /> 
+                       <XAxis dataKey="date" />
+                       <YAxis />
+                       <Tooltip />
+                       <Legend payload={[{id: 'positivity', value: 'Positivity of Cases based on testing numbers', type: 'square', color: '#B81D13'}]}/>
+                       <Line type="monotone" dataKey="positivity" stroke="#B81D13" />
+                    </LineChart>
+                </ResponsiveContainer>
+
+                
+
             </div>
         );
     }
---
+++ src/testingData.js
@@ -0,0 +1,3 @@
+export const testingData = [
+    {date: '11-01-2020', cases: 977, tests: 37133, positivity: 2.63},
+]
