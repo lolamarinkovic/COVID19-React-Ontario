277436d13b9b5ffb5e0c5b393923b3616ecd9dab
Lola Marinkovic <lola.marinkovic@ontariotechu.net>
2020-11-02 11:12:03 -0500

    made changes to legend

--- src/Chart.js
+++ src/Chart.js
@@ -15,19 +15,30 @@ class Chart extends React.Component {
                         <XAxis dataKey="date" />
                         <YAxis />
                         <Tooltip />
-                        <Legend />
-                        <Line type="monotone" dataKey="cases" stroke="#8884d8" />
+                        <Legend payload={[{id: 'cases', value: 'Number of cases', type: 'square', color: '#2e86c1'}]}/>
+                        <Line name="Cases:" type="monotone" dataKey="cases" stroke="#2e86c1" />
                     </LineChart>
                 </ResponsiveContainer>
 
                 <ResponsiveContainer width="80%" height={500}>
-                    <LineChart data={testingData} margin={{top: 50, right: 50, left: 20, bottom: 5}} >
+                    <LineChart data={testingData} margin={{top: 50, right: 50, left: 20, bottom: 5}}>
                        <CartesianGrid strokeDasharray="3 3" /> 
                        <XAxis dataKey="date" />
                        <YAxis />
                        <Tooltip />
-                       <Legend payload={[{id: 'positivity', value: 'Positivity of Cases based on testing numbers', type: 'square', color: '#B81D13'}]}/>
+                       <Legend payload={[{id: 'positivity', value: 'Percentage of positivity', type: 'square', color: '#B81D13'}]}/>
                        <Line type="monotone" dataKey="positivity" stroke="#B81D13" />
+                    </LineChart>
+                </ResponsiveContainer>
+
+                <ResponsiveContainer width="80%" height={500}>
+                    <LineChart data={testingData} margin={{top: 50, right: 50, left: 20, bottom: 5}}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="date" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend payload={[{id: 'tests', value: 'Number of tests', type: 'square', color: '#7d3c98'}]}/>
+                        <Line type="monotone" dataKey="tests" stroke="#7d3c98" />
                     </LineChart>
                 </ResponsiveContainer>
             </div>
