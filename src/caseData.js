fefc7560403820bbde711d5cbcbe73151469495c
Lola Marinkovic <lola.marinkovic@ontariotechu.net>
2020-11-06 10:45:04 -0500

    added values for november 6 2020

--- src/caseData.js
+++ src/caseData.js
@@ -269,5 +269,6 @@ export const caseData = [
     {cases: 948, date: '11-02-2020'},
     {cases: 1050, date: '11-03-2020'},
     {cases: 987, date: '11-04-2020'},
-    {cases: 998, date: '11-05-2020'}
+    {cases: 998, date: '11-05-2020'},
+    {cases: 1003, date: '11-06-2020'}
 ]
