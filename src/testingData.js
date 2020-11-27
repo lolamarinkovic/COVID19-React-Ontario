0dc0f4bee84cefa407ce50ba0034d0a910d96785
Lola Marinkovic <lola.marinkovic@ontariotechu.net>
2020-11-27 10:42:16 -0500

    added values for november 27 2020

--- src/testingData.js
+++ src/testingData.js
@@ -24,5 +24,6 @@ export const testingData = [
     {date: '11-23-2020', cases: 1589, tests: 37471, positivity: 4.24},
     {date: '11-24-2020', cases: 1009, tests: 27053, positivity: 3.73},
     {date: '11-25-2020', cases: 1373, tests: 36076, positivity: 3.81},
-    {date: '11-26-2020', cases: 1478, tests: 47576, positivity: 3.11}
+    {date: '11-26-2020', cases: 1478, tests: 47576, positivity: 3.11},
+    {date: '11-27-2020', cases: 1855, tests: 58037, positivity: 3.20}
 ]
