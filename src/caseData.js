43783dc3c387dabb7ce1e899e5c1176115abbaeb
Lola Marinkovic <lola.marinkovic@ontariotechu.net>
2020-12-16 11:03:27 -0500

    added data for december 16 2020

--- src/caseData.js
+++ src/caseData.js
@@ -309,5 +309,6 @@ export const caseData = [
     {cases: 1873, date: '12-12-2020'},
     {cases: 1677, date: '12-13-2020'},
     {cases: 1940, date: '12-14-2020'},
-    {cases: 2275, date: '12-15-2020'}
+    {cases: 2275, date: '12-15-2020'},
+    {cases: 2139, date: '12-16-2020'}
 ]
--- src/testingData.js
+++ src/testingData.js
@@ -43,5 +43,6 @@ export const testingData = [
     {date: '12-12-2020', cases: 1873, tests: 65260, positivity: 2.87},
     {date: '12-13-2020', cases: 1677, tests: 58190, positivity: 2.88},
     {date: '12-14-2020', cases: 1940, tests: 57091, positivity: 3.40},
-    {date: '12-15-2020', cases: 2275, tests: 39566, positivity: 5.75}
+    {date: '12-15-2020', cases: 2275, tests: 39566, positivity: 5.75},
+    {date: '12-16-2020', cases: 2139, tests: 49189, positivity: 4.35}
 ]
