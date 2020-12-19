b00d5171fddc6b325ba1351c106c99ab18090de0
Lola Marinkovic <lola.marinkovic@ontariotechu.net>
2020-12-19 14:54:55 -0500

    Updates

---
+++ precache-manifest.021f71539316c5829f0b6820517c7194.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "145986076b424fc258b71a964346adda",
+    "url": "/COVID19-React-Ontario/index.html"
+  },
+  {
+    "revision": "e43c7ecae6c118e15c2c",
+    "url": "/COVID19-React-Ontario/static/js/2.edf3e644.chunk.js"
+  },
+  {
+    "revision": "f3e91ced0874a3e507ef0e80f41dde1e",
+    "url": "/COVID19-React-Ontario/static/js/2.edf3e644.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e76d210425c456e7929",
+    "url": "/COVID19-React-Ontario/static/js/main.b7e95e5e.chunk.js"
+  },
+  {
+    "revision": "58da3d515d73a75e509e",
+    "url": "/COVID19-React-Ontario/static/js/runtime-main.0d58c86f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/COVID-19-React-Ontario/precache-manifest.deecc0d3514f4864994578f3040f7fe9.js"
+  "/COVID19-React-Ontario/precache-manifest.021f71539316c5829f0b6820517c7194.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/COVID-19-React-Ontario/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/COVID19-React-Ontario/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
