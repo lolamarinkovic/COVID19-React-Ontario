a23544e4a4cdeb18283ea31283f604d8cd7db248
Lola Marinkovic <lola.marinkovic@ontariotechu.net>
2021-02-07 17:33:36 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/COVID19-React-Ontario/precache-manifest.9c2470d5d56f64349b2f0cce27eaa8b2.js"
+  "/COVID19-React-Ontario/precache-manifest.f67390d7bba253359c4f1d03192b0046.js"
 );
 
 self.addEventListener('message', (event) => {
