f1f1044e8d1ec16f10a4339e13617c79007aba44
Lola Marinkovic <lola.marinkovic@ontariotechu.net>
2021-02-26 12:24:40 -0500

    deploy changes

---
+++ build/precache-manifest.0b79c3c266b2bf21eb1f785addc8c5b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62addf6ee4a3cd939b5133e7905066b4",
+    "url": "/COVID19-React-Ontario/index.html"
+  },
+  {
+    "revision": "f11bd7b391e2ae937946",
+    "url": "/COVID19-React-Ontario/static/js/2.6619e194.chunk.js"
+  },
+  {
+    "revision": "f3e91ced0874a3e507ef0e80f41dde1e",
+    "url": "/COVID19-React-Ontario/static/js/2.6619e194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d28b3ad41bd4f2b715ed",
+    "url": "/COVID19-React-Ontario/static/js/main.a2f4f70c.chunk.js"
+  },
+  {
+    "revision": "58da3d515d73a75e509e",
+    "url": "/COVID19-React-Ontario/static/js/runtime-main.0d58c86f.js"
+  }
+]);
